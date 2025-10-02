88350beecdb378cbbdfccf52b97fb0d7528b5589
8allenY <105749487+8allenY@users.noreply.github.com>
2025-10-02 17:16:14 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,5 +10,12 @@ bot = Bot(token=TELEGRAM_TOKEN)
 async def test_bot():
     await bot.send_message(chat_id=CHANNEL_ID, text="✅ Bot is alive!")
 
+async def startup():
+    try:
+        await main()
+    finally:
+        await bot.session.close()
+
 if __name__ == "__main__":
-    asyncio.run(test_bot())
+    asyncio.run(startup())
+
